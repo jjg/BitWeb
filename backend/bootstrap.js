3e671c3ca3ac56d48c94b1d3ad0335cbb197596c
jjg <jason.gullickson@gmail.com>
2013-08-31 10:50:38 -0500

    fleshing out the bootstrap code

---
+++ backend/bootstrap.js
@@ -0,0 +1,12 @@
+// start listening for incoming connections
+
+// handle incoming request
+
+// if host is in the cache, make sure it's started and hand-off the request
+
+// if the host isn't in the cache, try to find a namecoin and DNS entry
+
+// hand the request off to whichever comes back first
+
+// if the host is found on namecoin, setup a local cached copy
+
